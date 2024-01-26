f754e6f5a81fa280068e07e02d641a9dccaa4383
eduardo <marketingdoeduardo@gmail.com>
2024-01-25 21:22:55 -0300

    Terminado o Projeto

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,27 @@
 AOS.init();
+
+const dataDoAniversario = new Date("jan 31, 2024");
+const timeStampDoAnivessario = dataDoAniversario.getTime();
+
+const time = setInterval(function() {
+    const dataAtual = new Date();
+    const timeStampAtual = dataAtual.getTime();
+
+    const distanciaAteOEvento = timeStampDoAnivessario - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaEmMs / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById("contador").innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(time);
+        document.getElementById("expirado").innerHTML = `Evento expirado`;
+    }
+}, 1000);
